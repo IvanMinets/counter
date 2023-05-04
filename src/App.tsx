96877505bd0e119f6b17bb2558bb5778e4a56762
Ivan Minets2 <minetssmash@gmail.com>
2023-05-04 18:21:38 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    const [counter, setCounter] = useState<number>(0);
+    const [count, setCount] = useState<number>(0);
 
     const increaseCount = () => {
-        if (counter < 5) {
-            setCounter(counter + 1);
+        if (count < 5) {
+            setCount(count + 1);
         }
     };
     const resetCount = () => {
-        setCounter(0)
+        setCount(0)
     };
 
     return (
         <div className="App">
-            <div className={counter === 5 ? "ValueMaxCheck" : 'ValueCheck'}>{counter}</div>
-            <button className={"IncreaseButton"} disabled={counter === 5} onClick={increaseCount}>increase</button>
-            <button className={"ResetButton"} disabled={counter !== 5} onClick={resetCount}>reset</button>
+            <div className={count === 5 ? "ValueMaxCheck" : 'ValueCheck'}>{count}</div>
+            <button className={"IncreaseButton"} disabled={count === 5} onClick={increaseCount}>increase</button>
+            <button className={"ResetButton"} disabled={count !== 5} onClick={resetCount}>reset</button>
         </div>
     );
 }

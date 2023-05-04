2d70e602f54bd8357a4ba19c0a0114500b902ae6
Ivan Minets2 <minetssmash@gmail.com>
2023-05-04 18:04:52 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState<number>(0);
+
+    const increaseCount = () => {
+        if (counter < 5) {
+            setCounter(counter + 1);
+        }
+    };
+    const resetCount = () => {
+        setCounter(0)
+    };
+
+    return (
+        <div className="App">
+            <div className={counter === 5 ? "ValueMaxCheck" : 'ValueCheck'}>{counter}</div>
+            <button className={"IncreaseButton"} disabled={counter === 5} onClick={increaseCount}>increase</button>
+            <button className={"ResetButton"} disabled={counter !== 5} onClick={resetCount}>reset</button>
+        </div>
+    );
 }
 
 export default App;
